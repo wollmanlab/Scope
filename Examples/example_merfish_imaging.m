db294d06d98f0c76749acca6999ac44c1c8540b3
Wollman Lab code repos <wollmanlab@ucla.edu>
2023-08-18 10:20:58 -0700

    Update example_merfish_imaging.m

--- Examples/example_merfish_imaging.m
+++ Examples/example_merfish_imaging.m
@@ -1,35 +1,41 @@
 %% Initialize Scope
-Scp = RamboScope;
+Scp = BlueScope;
 %% Double check that 000 is the top left corner of the scope and Lower Limit
 % Scp.Z = 0;
 % Scp.XY = [0 0];
 %% Set up: username, project name and dataset
 Scp.Username = 'Zach'; % your username!
-Scp.Project = 'Test'; % the project this dataset correspond to
-Scp.Dataset = 'TBI'; % the name of this specific image dataset - i.e. this experiment.
+Scp.Project = 'Elegans'; % the project this dataset correspond to
+Scp.Dataset = 'Test'; % the name of this specific image dataset - i.e. this experiment.
 Scp.ExperimentDescription = ['500 ug TBI Probes/brain'];
 
 %% Setup Imaging Parameters
 % For Data Collection
 Scp.FlowData.AcqData = AcquisitionData;
 Scp.FlowData.AcqData(1).Channel = 'DeepBlue';
-Scp.FlowData.AcqData(1).Exposure = 10; %
+Scp.FlowData.AcqData(1).Exposure = 20; %
 Scp.FlowData.AcqData(1).Delay = 10; %
 Scp.FlowData.AcqData(2).Channel = 'FarRed';
-Scp.FlowData.AcqData(2).Exposure = 500; %
+Scp.FlowData.AcqData(2).Exposure = 2500; %
 Scp.FlowData.AcqData(2).Delay = 10; %
 
 % For Preview
 preview_acqdata = AcquisitionData;
 preview_acqdata(1).Channel = 'DeepBlue';
-preview_acqdata(1).Exposure = 20; %
+preview_acqdata(1).Exposure = 50; %
 preview_acqdata(1).Delay = 10; %
+
+photobleach_acqdata = AcquisitionData;
+photobleach_acqdata(1).Channel = 'FarRed';
+photobleach_acqdata(1).Exposure = 5000; %
+photobleach_acqdata(1).Delay = 10; %
 %%
 Scp.FlowData.start_Gui()
 Scp.FlowData.start_Fluidics()
 
 %% Setup Fluidics Parameters
-Scp.FlowData.Rounds = [1:18,25];
+Scp.FlowData.Rounds = [25,1:18];
+Scp.FlowData.Rounds = [1:4];
 Scp.FlowData.FlowGroups = {'M'};
 Scp.FlowData.Protocols = {'ClosedStripHybeImage'};
 Scp.FlowData.update_FlowData();
@@ -43,12 +49,12 @@ for c=1: Scp.FlowData.n_coverslips
     coverslip = Scp.FlowData.coverslips{c};
     Wells = {coverslip};
     % Plate only if sample is in center
-    Scp.createPositions('spacing',1, ...
-        'sitesperwell',[30,30], ...
-        'sitesshape','grid', ...
-        'wells',Wells,'optimize',true)
+%     Scp.createPositions('spacing',1, ...
+%         'sitesperwell',[30,30], ...
+%         'sitesshape','grid', ...
+%         'wells',Wells,'optimize',true)
     % MM if sample is not in center
-%     Scp.createPositionFromMM()
+    Scp.createPositionFromMM()
     Scp.Pos.Well = coverslip;
     Scp.acquire(preview_acqdata);
     position_acq_names{c} = Scp.getLastAcqname;
@@ -59,10 +65,11 @@ for c=1:Scp.FlowData.n_coverslips
     coverslip = Scp.FlowData.coverslips{c};
     Scp.Pos = Scp.Pos.load([coverslip]);
     Wells = {coverslip};
-    Scp.createPositions('spacing',1, ...
-        'sitesperwell',[30,30], ...
-        'sitesshape','grid', ...
-        'wells',Wells,'optimize',true)
+%     Scp.createPositions('spacing',1, ...
+%         'sitesperwell',[30,30], ...
+%         'sitesshape','grid', ...
+%         'wells',Wells,'optimize',true)
+    Scp.createPositionFromMM()
     Scp.Pos.Well = coverslip;
     Scp.filterPositionsByDraw('acq_name',position_acq_names{c},'acqdata',preview_acqdata)
     Scp.Pos.save
@@ -72,8 +79,8 @@ for c=1:Scp.FlowData.n_coverslips
     coverslip = Scp.FlowData.coverslips{c};
     Scp.Pos = Scp.Pos.load([coverslip]);
     Scp.AF = ContrastPlaneFocus;
-    Scp.AF.channel = 'AutoFocus';%'AutoFocus';
-    Scp.AF.exposure = 10;
+    Scp.AF.channel = 'AutoFocus';
+    Scp.AF.exposure = 100;
     Scp.AF.coarse_window = 50;
     Scp.AF.coarse_dZ = 5;
     Scp.AF.medium_window = 15;
@@ -98,30 +105,55 @@ for c=1:Scp.FlowData.n_coverslips
     Scp.Pos.save
     Scp.AF.save
 end
+
+%% Photobleach
+photobleach_acqdata = AcquisitionData;
+photobleach_acqdata(1).Channel = 'FarRed';
+photobleach_acqdata(1).Exposure = 5000; %
+photobleach_acqdata(1).Delay = 10; %
+Scp.acquire(photobleach_acqdata)
 %%
 dZ_min = -5;
-dZ_max = 5;
-dZ_step = 0.4;
+dZ_max = 30;
+dZ_step = 1;%0.4;
 dZ_steps = linspace(dZ_min,dZ_max,ceil((dZ_max-dZ_min)/dZ_step));
 
 %% Collect Data
+Scp.FlowData.AcqData = AcquisitionData;
+Scp.FlowData.AcqData(1).Channel = 'DeepBlue';
+Scp.FlowData.AcqData(1).Exposure = 20; %
+Scp.FlowData.AcqData(1).Delay = 10; %
+Scp.FlowData.AcqData(2).Channel = 'FarRed';
+Scp.FlowData.AcqData(2).Exposure = 2500; %
+Scp.FlowData.AcqData(2).Delay = 10; %
 Scp.AutoFocusType='hardware';
-for i=2:size(Scp.FlowData.Tasks,1)
+for i=4:size(Scp.FlowData.Tasks,1)
     Scp.FlowData.current_idx = i;
     % FLOW
     Scp.FlowData.flow(Scp);
+    %pause(60)
     % IMAGE
-    for c = 1:length(Scp.FlowData.wells)
-        coverslip = Scp.FlowData.coverslips(c);
+    for c = 1:length(Scp.FlowData.image_wells)
+        coverslip = Scp.FlowData.coverslips{c};
         Scp.Pos = Scp.Pos.load([coverslip]);
         Scp.AF = Scp.AF.load([coverslip]);
         % Update AutoFocus
         Scp.AF = Scp.AF.updateZ(Scp);
         % Image
         Scp.acquire(Scp.FlowData.AcqData, ...
-            'baseacqname',['Hybe',Scp.FlowData.other], ...
+            'baseacqname',['Hybe',Scp.FlowData.image_other], ...
             'dz',dZ_steps)
         Scp.Pos.save
         Scp.AF.save
     end
 end
+%%
+Scp.FlowData.AcqData = AcquisitionData;
+Scp.FlowData.AcqData(1).Channel = 'DeepBlue';
+Scp.FlowData.AcqData(1).Exposure = 100; %
+Scp.FlowData.AcqData(1).Delay = 10; %
+Scp.AutoFocusType='hardware';
+% Scp.AF = Scp.AF.updateZ(Scp);
+Scp.acquire(Scp.FlowData.AcqData, ...
+            'baseacqname',['nucstain'], ...
+            'dz',dZ_steps)
