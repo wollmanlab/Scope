38007bbcb21057e746c51d1c0c29b6553526c481
Wollman Lab code repos <wollmanlab@ucla.edu>
2023-09-19 13:53:06 -0700

    Update example_merfish_imaging.m

--- Examples/example_merfish_imaging.m
+++ Examples/example_merfish_imaging.m
@@ -4,9 +4,9 @@ Scp = BlueScope;
 % Scp.Z = 0;
 % Scp.XY = [0 0];
 %% Set up: username, project name and dataset
-Scp.Username = 'Zach'; % your username!
-Scp.Project = 'Elegans'; % the project this dataset correspond to
-Scp.Dataset = 'Test'; % the name of this specific image dataset - i.e. this experiment.
+Scp.Username = 'Haley'; % your username!
+Scp.Project = 'TBI'; % the project this dataset correspond to
+Scp.Dataset = 'DNA'; % the name of this specific image dataset - i.e. this experiment.
 Scp.ExperimentDescription = ['500 ug TBI Probes/brain'];
 
 %% Setup Imaging Parameters
@@ -21,7 +21,7 @@ Scp.FlowData.AcqData(2).Delay = 10; %
 
 % For Preview
 preview_acqdata = AcquisitionData;
-preview_acqdata(1).Channel = 'DeepBlue';
+preview_acqdata(1).Channel = 'FarRed';
 preview_acqdata(1).Exposure = 50; %
 preview_acqdata(1).Delay = 10; %
 
@@ -35,9 +35,10 @@ Scp.FlowData.start_Fluidics()
 
 %% Setup Fluidics Parameters
 Scp.FlowData.Rounds = [25,1:18];
-Scp.FlowData.Rounds = [1:4];
+% Scp.FlowData.Rounds = [1:4];
 Scp.FlowData.FlowGroups = {'M'};
 Scp.FlowData.Protocols = {'ClosedStripHybeImage'};
+Scp.FlowData.ImageProtocols = {'ClosedStripHybeImage'};
 Scp.FlowData.update_FlowData();
 Scp.FlowData.Tasks
 %% Acquire All Wells First Find Focus manually First
@@ -79,6 +80,7 @@ for c=1:Scp.FlowData.n_coverslips
     coverslip = Scp.FlowData.coverslips{c};
     Scp.Pos = Scp.Pos.load([coverslip]);
     Scp.AF = ContrastPlaneFocus;
+    Scp.AF.use_groups = false;
     Scp.AF.channel = 'AutoFocus';
     Scp.AF.exposure = 100;
     Scp.AF.coarse_window = 50;
@@ -105,18 +107,53 @@ for c=1:Scp.FlowData.n_coverslips
     Scp.Pos.save
     Scp.AF.save
 end
+%%
+dZ_min = 5;
+dZ_max = 20;
+dZ_step = 0.4;
+dZ_steps = linspace(dZ_min,dZ_max,ceil((dZ_max-dZ_min)/dZ_step))
+%% Image PolyT
+Scp.FlowData.AcqData = AcquisitionData;
+Scp.FlowData.AcqData(1).Channel = 'DeepBlue';
+Scp.FlowData.AcqData(1).Exposure = 20; %
+Scp.FlowData.AcqData(1).Delay = 10; %
+Scp.FlowData.AcqData(2).Channel = 'FarRed';
+Scp.FlowData.AcqData(2).Exposure = 100; %
+Scp.FlowData.AcqData(2).Delay = 10; %
+Scp.AutoFocusType='hardware';
+for i=2:2
+    Scp.FlowData.current_idx = i;
+    % FLOW
+    Scp.FlowData.flow(Scp);
+    %pause(60)
+    % IMAGE
+    for c = 1:length(Scp.FlowData.image_wells)
+        coverslip = Scp.FlowData.image_wells(c);
+        Scp.Pos = Scp.Pos.load([coverslip]);
+        Scp.AF = Scp.AF.load([coverslip]);
+        % Update AutoFocus
+        Scp.AF = Scp.AF.updateZ(Scp);
+        % Image
+        Scp.acquire(Scp.FlowData.AcqData, ...
+            'baseacqname',['Hybe',Scp.FlowData.image_other], ...
+            'dz',dZ_steps)
+        Scp.Pos.save
+        Scp.AF.save
+    end
+end
 
-%% Photobleach
+%% Photobleach (flow in tbs)
+command = Scp.FlowData.build_command('ClosedValve',['M'],'TBS+3');
+Scp.FlowData.send_command(command,Scp)
+Scp.FlowData.wait_until_available()
+
 photobleach_acqdata = AcquisitionData;
 photobleach_acqdata(1).Channel = 'FarRed';
 photobleach_acqdata(1).Exposure = 5000; %
 photobleach_acqdata(1).Delay = 10; %
-Scp.acquire(photobleach_acqdata)
-%%
-dZ_min = -5;
-dZ_max = 30;
-dZ_step = 1;%0.4;
-dZ_steps = linspace(dZ_min,dZ_max,ceil((dZ_max-dZ_min)/dZ_step));
+for i=1:5
+    Scp.acquire(photobleach_acqdata)
+end
 
 %% Collect Data
 Scp.FlowData.AcqData = AcquisitionData;
@@ -127,14 +164,14 @@ Scp.FlowData.AcqData(2).Channel = 'FarRed';
 Scp.FlowData.AcqData(2).Exposure = 2500; %
 Scp.FlowData.AcqData(2).Delay = 10; %
 Scp.AutoFocusType='hardware';
-for i=4:size(Scp.FlowData.Tasks,1)
+for i=24:24%:size(Scp.FlowData.Tasks,1)
     Scp.FlowData.current_idx = i;
     % FLOW
     Scp.FlowData.flow(Scp);
     %pause(60)
     % IMAGE
     for c = 1:length(Scp.FlowData.image_wells)
-        coverslip = Scp.FlowData.coverslips{c};
+        coverslip = Scp.FlowData.image_wells(c);
         Scp.Pos = Scp.Pos.load([coverslip]);
         Scp.AF = Scp.AF.load([coverslip]);
         % Update AutoFocus
@@ -150,10 +187,10 @@ end
 %%
 Scp.FlowData.AcqData = AcquisitionData;
 Scp.FlowData.AcqData(1).Channel = 'DeepBlue';
-Scp.FlowData.AcqData(1).Exposure = 100; %
+Scp.FlowData.AcqData(1).Exposure = 500; %
 Scp.FlowData.AcqData(1).Delay = 10; %
 Scp.AutoFocusType='hardware';
-% Scp.AF = Scp.AF.updateZ(Scp);
+Scp.AF = Scp.AF.updateZ(Scp);
 Scp.acquire(Scp.FlowData.AcqData, ...
             'baseacqname',['nucstain'], ...
             'dz',dZ_steps)
